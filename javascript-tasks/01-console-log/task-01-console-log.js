5472b6d834eaf1ad26332dac4e676de5d1afc8a5
Martijn Kuik <77934777+martijnkuik@users.noreply.github.com>
2024-07-29 22:25:44 +0200

    Update task-01-console-log.js

--- javascript-tasks/01-console-log/task-01-console-log.js
+++ javascript-tasks/01-console-log/task-01-console-log.js
@@ -1,2 +1,6 @@
+console.log("Paris 2024 Olympics: prediction of winners in  Men's BMX freestyle")
+console.log()
+console.log("Gold : Anthony Jeanjean")
+console.log("Silver: Kieran Reilly")
+console.log("Bronze: Marin Rante")
 
-console.log("BMX")
