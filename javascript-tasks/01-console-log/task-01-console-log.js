5e41018804731e5fab3680ac9ad374c057469687
Martijn Kuik <77934777+martijnkuik@users.noreply.github.com>
2024-07-30 22:51:17 +0200

    Update task-01-console-log.js

--- javascript-tasks/01-console-log/task-01-console-log.js
+++ javascript-tasks/01-console-log/task-01-console-log.js
@@ -1,5 +1,12 @@
+/* 
+Prediction of medal winners,
+done during the Codedex hackathon. 
+*/ 
+
 console.log("Paris 2024 Olympics: prediction of winners in  Men's BMX freestyle");
-console.log();
+
+console.log(); // Blank Line
+
 console.log("🥇Anthony Jeanjean");
 console.log("🥈Kieran Reilly");
 console.log("🥉Marin Rante");
