2a28fb35ba061e2aeb13686a2aa472997adab04c
Martijn Kuik <77934777+martijnkuik@users.noreply.github.com>
2024-08-02 23:38:17 +0200

    Update task-02-comments.js

--- javascript-tasks/02-comments/task-02-comments.js
+++ javascript-tasks/02-comments/task-02-comments.js
@@ -1,4 +1,5 @@
-/* Prediction of medal winners was made during the Codedex hackathon. */
+/* Prediction of medal winners was made during the Codedex hackathon.
+The top three competitors who scored the highest in the forecast. */
 
 console.log("Paris 2024 Olympics: predictions of winners in Men's BMX freestyle");
 console.log()//Blank-line
