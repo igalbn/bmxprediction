8924cf91a95118a0b9c07edfbcbd2cc4ba4309d5
Martijn Kuik <77934777+martijnkuik@users.noreply.github.com>
2024-08-18 23:10:17 +0200

    Update task-04-constants.js

--- javascript-tasks/04-constants/task-04-constants.js
+++ javascript-tasks/04-constants/task-04-constants.js
@@ -1,1 +1,22 @@
-//new file
+console.log("Medals winners Men's BMX freestyle Olympics 2020 Tokyo");
+
+console.log();
+
+
+let goldWinner   = "Logan Martin  93.30";
+let silverWinner = "Daniel Dhers  92.05";
+let bronzeWinner = "Declan Brooks 90.80";
+
+const GOLD_MEDAL = "🥇"
+const SILVER_MEDAL = "🥈"
+const BRONZE_MEDAL = "🥉"
+
+const DECORATION_LINE = '**********************';
+
+console.log(DECORATION_LINE);
+console.log(GOLD_MEDAL, goldWinner);
+console.log(DECORATION_LINE);
+console.log(SILVER_MEDAL, silverWinner);
+console.log(DECORATION_LINE)
+console.log(BRONZE_MEDAL, bronzeWinner);
+console.log(DECORATION_LINE)
